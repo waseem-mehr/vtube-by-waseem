a36eb72ac5cd4d4ec1d56a8a48bf006f6298e673
Waseem Mehr <54299629+waseem-mehr@users.noreply.github.com>
2020-07-17 10:28:44 +0500

    Update settings.py

--- vTube/settings.py
+++ vTube/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT=os.path.join(BASE_DIR,'/staticfiles')
-STATICFILES_STORAGE="whitenoise.django.GzipManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 print(BASE_DIR)
 
 os.makedirs(STATIC_ROOT, exist_ok=True)
@@ -131,4 +131,15 @@ STATICFILES_DIRS = [
 ]
 MEDIA_URL='media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 django_heroku.settings(locals())
